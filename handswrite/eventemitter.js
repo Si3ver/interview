cba1551144deb9db450452136bb218f01b7fd1d5
alwynzhou <alwynzhou@webank.com>
2021-03-04 20:26:51 +0800

    come true eventEmitter

--- handswrite/eventemitter.js
+++ handswrite/eventemitter.js
@@ -3,18 +3,36 @@ class MyEventEmitter {
     this._cache = Object.create(null)
   }
   on (type, callback) {
+    if (this._cache[type]) {
+      this._cache[type].push(callback)
+    } else {
+      this._cache[type] = [callback]
+    }
     
   }
   off (type, callback) {
-
+    let fns = this._cache[type]
+    if (callback) {
+      this._cache[type] = fns.filter(fn => fn !== callback)
+    } else {
+      this._cache[type] = []
+    }
   }
   trigger (type, data) {
-
+    const fns = this._cache[type]
+    if (Array.isArray(fns)) {
+      fns.forEach(fn => fn(data))
+    }
   }
 }
 
 // ---- test case ----
+const cb1 = x => {console.log(x)}
+const cb2 = x => {console.log(x + x)}
 const em = new MyEventEmitter()
-em.on('test', console.log)
-em.trigger('test', 'helloworld')
-
+em.on('hEvent', cb1)
+em.on('hEvent', cb2)
+em.off('hEvent', cb1)
+em.trigger('hEvent', 'helloworld')
+em.off('hEvent')
+em.trigger('hEvent', 'helloworld')
