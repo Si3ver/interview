77b31a18f376105ee218d522c3c8acf038a41eda
Si3ver <izhouwl@163.com>
2021-03-11 15:27:05 +0800

    尾递归优化,默认不开启~~~

---
+++ recur/node.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// 计算1-N的累加值（尾递归）
+function f(n, sum = 1) {
+  if (n <= 1) {
+      return sum;
+  }
+  return f(n - 1, sum + n);
+}
+const result = f(100000);
+console.log(result);
